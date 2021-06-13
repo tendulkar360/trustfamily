8109c8a940761ee1f404cbf4fc25994721855ad5
Tamim Ahmed <78292363+tendulkar360@users.noreply.github.com>
2021-06-13 21:34:48 +0600

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 const menubtn = document.getElementById('menu');
 const closebtn = document.getElementById('close');
 const ul = document.getElementById('ul');
+const btt = document.getElementById('btt');
+const body = document.getElementById('body');
 
 menubtn.addEventListener("click",function(){
     ul.style.right = "0";
@@ -8,4 +10,16 @@ menubtn.addEventListener("click",function(){
 
 closebtn.addEventListener("click",function(){
     ul.style.right = "-260px";
+})
+
+window.addEventListener("scroll",function(){
+    if(window.scrollY > 100){
+        btt.style = "bottom: 15px; display: block;";
+    }else{
+        btt.style = "bottom: -15px; display: none;";
+    }
+})
+
+btt.addEventListener("click", function(){
+    window.scroll = 0;
 })
